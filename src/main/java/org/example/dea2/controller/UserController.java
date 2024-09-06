6c57e4d462836cf4b4b06ef051a7dffaff062e35
Senuk27 <senukthiyasara@gmail.com>
2024-09-06 23:15:36 +0530

    modifications

---
+++ src/main/java/org/example/dea2/controller/UserController.java
@@ -0,0 +1,27 @@
+package org.example.dea2.controller;
+
+import org.example.dea2.dto.request.LoginApiRequest;
+import org.example.dea2.dto.request.SignUpApiRequest;
+import org.example.dea2.service.UserService;
+import org.example.dea2.dto.response.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/user")
+
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping("/registration")
+    public ApiResponse userSignUp(@RequestBody SignUpApiRequest request) {
+        return userService.userSignUp(request);
+
+    }
+
+    @PostMapping("/login")
+    public ApiResponse userLogin(@RequestBody LoginApiRequest request) {
+        return userService.userLogin(request);
+    }
+}
